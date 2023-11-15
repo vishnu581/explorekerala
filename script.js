d12484cb89cb2f1f59dd8cf3984431f77a4c07e1
vishnu581 <vishnuvenugopal581@gmail.com>
2023-11-15 18:58:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let user1 = document.querySelector(".data1");
+let user2 = document.querySelector(".data2");
+
+user2.style.display = "none";
+user1.style.display = "block";
+
+function showhide(){
+    user2.style.display = "none";
+    user1.style.display = "block";
+
+
+}
+function showhide1(){
+    user1.style.display = "none";
+
+    user2.style.display = "block";
+ 
+   
+}
+function message1(){
+
+    alert('hiiii');
+}
