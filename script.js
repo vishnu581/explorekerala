476690c8c80931690844d22c8337f5bb1d8c0b7b
vishnu581 <vishnuvenugopal581@gmail.com>
2023-11-17 15:10:52 +0530

    day 3 almost completed the portotype

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let user1 = document.querySelector(".data1");
-let user2 = document.querySelector(".data2");
+let user1 = document.querySelector(".show1");
+let user2 = document.querySelector(".show2");
+let user3 = document.querySelector(".show3");
 
 user2.style.display = "none";
 user1.style.display = "block";
@@ -7,11 +8,14 @@ user1.style.display = "block";
 function showhide(){
     user2.style.display = "none";
     user1.style.display = "block";
+    user3.style.display = "block";
 
 
 }
 function showhide1(){
     user1.style.display = "none";
+    user3.style.display = "none";
+
 
     user2.style.display = "block";
  
